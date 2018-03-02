840518abe7b3f4ff0d051c58d6e4d6f80ac593d0
Peter Sprygada <psprygad@redhat.com>
2018-03-02 11:58:31 -0500

    fixes issue with cli_get module
    The local action plugin had the wrong base class defined and therefore
    wasn't running at all.  This change fixes the bug.

--- action_plugins/cli_get.py
+++ action_plugins/cli_get.py
@@ -9,7 +9,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from ansible.plugins.action import ActionBase
+from ansible.plugins.action.normal import ActionModule as ActionBase
 from ansible.errors import AnsibleError
 
 
