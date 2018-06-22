5e50da26fbc1864c2b9cd6fb14d7183359054b3a
Deepak Agrawal <deepacks@gmail.com>
2018-06-22 10:00:34 +0000

    working model for downloading all schemas

--- action_plugins/fetch_schema.py
+++ action_plugins/fetch_schema.py
@@ -12,6 +12,12 @@ import json
 import collections
 import yaml
 import sys
+is_py2 = sys.version[0] == '2'
+if is_py2:
+    import Queue as queue
+else:
+    import queue as queue
+
 import q
 
 from ansible import constants as C
@@ -93,7 +99,7 @@ class SchemaStore(object):
     def get_one_schema(self, schema_id):
         if self._all_schema_list == None:
 	    self.get_schema_description()
-        
+
         found = False
         # Search for schema that are supported by device.
         # Also get namespace for retreival
@@ -101,7 +107,8 @@ class SchemaStore(object):
         for index, schema_list in enumerate(self._all_schema_list):
             if to_bytes(schema_id) == to_bytes(schema_list["identifier"],
                                             errors='surrogate_or_strict'):
-                schema_cache_entry["id"] = schema_id
+                schema_cache_entry["id"] = to_bytes(schema_id,
+                                 errors='surrogate_or_strict')
                 schema_cache_entry["ns"] = self._all_schema_list[index]["namespace"]
                 schema_cache_entry["format"] = self._all_schema_list[index]["format"]
                 found = True
@@ -133,18 +140,68 @@ class SchemaStore(object):
                 f.write(data_model)
             self._schema_cache.append(schema_cache_entry)
 
-        q(self._schema_cache, found, data_model)
         return (found, data_model)
 
     def check_schema_exist(self, schema_id):
         found = False
-        for schema_cache_entry in self._schema_cache:
-            if schema_id == schema_cache_entry["id"]:
-                found = True
-                break
-        return found
-          
+        yang_file_name = schema_id + '.yang'
+        abs_file_name = os.path.join(self._folder, yang_file_name)
+        if os.path.exists(abs_file_name):
+            found = True
 
+        return found
+
+    def get_dependant_schema_from_yang_file(self, yang_fname):
+        if not os.path.exists(yang_fname):
+           raise ValueError('path specified in src not found')
+
+        with open(yang_fname, 'r') as f:
+            data_model = to_text(f.read(), errors='surrogate_or_strict')
+
+        importre = re.compile(r'import (.+) {')
+        dep_data_model_list = importre.findall(data_model)
+        return dep_data_model_list
+
+    def get_schema_and_dependants(self, schema_id):
+        try:
+            found, data_model = self.get_one_schema(schema_id)
+        except ValueError as exc:
+            raise ValueError(exc)
+
+        if found:
+            yang_file_name = schema_id + '.yang'
+            abs_file_name = os.path.join(self._folder, yang_file_name)
+            try:
+                dep_list = self.get_dependant_schema_from_yang_file(abs_file_name)
+            except ValueError as exc:
+                raise ValueError(exc)
+            return dep_list
+        else:
+            return None
+
+    def run(self, schema_id):
+        changed = False
+        counter = 0
+        sq = queue.Queue()
+        sq.put(schema_id)
+
+        while (sq.empty() != True):
+            schema_id = sq.get()
+            if self.check_schema_exist(schema_id):
+               q('found %s' % schema_id)
+               continue
+            try:
+                schema_dlist = self.get_schema_and_dependants(schema_id)
+            except ValueError as e:
+                raise ValueError(e)
+            for schema in schema_dlist:
+                if not self.check_schema_exist(schema):
+                    q('not found %s' % schema)
+                    sq.put(schema)
+                    changed = True
+                    counter = counter + 1
+
+        return (changed, counter)
 
 class ActionModule(ActionBase):
 
@@ -163,19 +220,34 @@ class ActionModule(ActionBase):
         dest_folder = self._task.args.get('destination')
         if dest_folder == None:
             dest_folder = self._get_default_folder()
+        else:
+            dest_folder = to_bytes(dest_folder, errors='surrogate_or_strict')
+            dest_folder = unfrackpath(dest_folder)
 
         socket_path = self._connection.socket_path
         conn = Connection(socket_path)
 
         ss = SchemaStore(conn, dest_folder)
 
+        result_list = []
         try:
            for schema in schemas:
-              ss.get_one_schema(schema)
+              changed, counter = ss.run(schema)
+              q(changed, counter)
+              result_list.append((changed, counter))
         except ValueError as exc:
            return dict(failed=True, msg=to_text(exc))
-        
-        result["changed"] = True
+
+        final_re = False
+        final_count = 0
+        for re, counter in result_list:
+            if re == True:
+               final_re = True
+            final_count = final_count + counter
+
+        result["changed"] = final_re
+        result["schema_folder_path"] = dest_folder
+        result["number_schema_fetched"] = final_count
         return result
 
     def _get_working_path(self):
