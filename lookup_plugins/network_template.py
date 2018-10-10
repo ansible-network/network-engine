e135c7ec18d226d71c60f9f13510194924e5f444
Paul Belanger <pabelanger@redhat.com>
2018-10-10 13:28:36 -0400

    Clean up F841,H flake8 warnings
    This fixes the following warnings:
      F841 local variable '...' is assigned to but never used
    
    We can also remove H here too, it doesn't break anything.
    
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- lookup_plugins/network_template.py
+++ lookup_plugins/network_template.py
@@ -46,10 +46,6 @@ from ansible.errors import AnsibleError, AnsibleUndefinedVariable
 class LookupModule(LookupBase):
 
     def run(self, terms, variables, **kwargs):
-
-        convert_data_p = kwargs.get('convert_data', True)
-        lookup_template_vars = kwargs.get('template_vars', {})
-
         self.ds = variables.copy()
 
         config_lines = list()
@@ -61,14 +57,11 @@ class LookupModule(LookupBase):
             display.vvvv("File lookup using %s as file" % lookupfile)
 
             if lookupfile:
-                with open(to_bytes(lookupfile, errors='surrogate_or_strict'), 'rb') as f:
-                    template_data = to_text(f.read(), errors='surrogate_or_strict')
-
+                with open(to_bytes(lookupfile, errors='surrogate_or_strict'), 'rb'):
                     tasks = self._loader.load_from_file(lookupfile)
 
                     for task in tasks:
-                        name = task.pop('name', None)
-
+                        task.pop('name', None)
                         register = task.pop('register', None)
 
                         when = task.pop('when', None)
@@ -120,9 +113,8 @@ class LookupModule(LookupBase):
 
         for entry in block:
             task = entry.copy()
-
-            name = task.pop('name', None)
-            register = task.pop('register', None)
+            task.pop('name', None)
+            task.pop('register', None)
 
             when = task.pop('when', None)
             if when is not None:
